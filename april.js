f6ef648bd912d4d40acb89889664298b858b3c73
Dhunter1017 <2012dhunter@gmail.com>
2021-03-13 14:20:27 -0500

    Added Two javascript functions

---
+++ april.js
@@ -0,0 +1,13 @@
+var showAlert = function (){
+    alert("Hello World!");
+};
+
+showAlert();
+
+var sayHello = function (firstName) {
+    alert("Hello " + firstName);
+};
+
+showAlert();
+
+sayHello("Kasey");
---
+++ march.js
@@ -0,0 +1,7 @@
+function showName(firstName, lastName) {
+    alert("Hello " + firstName + " " + lastName);
+}
+
+showName("Kasey", "Hunter");
+showName("David", "Hunter");
+showName(09, 17);
